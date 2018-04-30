06c1bb2f25903aa0277c3acce3a7b4380d725c5a
songhaow <songhaow@hotmail>
2018-04-30 09:55:22 +0800

    initial commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles =[
+        {
+            'id':1,
+            'title': 'Article 1',
+            'body': 'Lorem xxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXX',
+            'author': 'S. Wang',
+            'create_date': '03-23-2018'
+        },
+        {
+            'id':2,
+            'title': 'Article 2',
+            'body': 'Lorem xxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXX',
+            'author': 'J. Wang',
+            'create_date': '03-25-2018'
+        },
+        {
+            'id':3,
+            'title': 'Article 3',
+            'body': 'Lorem xxxxxxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXX',
+            'author': 'J. Wang',
+            'create_date': '03-28-2018'
+        }
+    ]
+    return articles
